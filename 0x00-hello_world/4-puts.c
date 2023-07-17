bcf0105076a3dd5272771a864feb169a6f150fb6
Nadirovish <Nadirsekkat1@gmail.com>
2023-07-17 13:55:36 -0700

    hello

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+/**
+ * the main function will retun a 0 value in the end of the program
+ * the function puts will be used to write a given line
+*/
+#include<stdio.h>
+int main()
+{
+	puts(Programming is like building a multilingual puzzle);
+	return(0);
+}
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,9 @@
-# < stdio.h >
-int main(void)/* learn coding */
+#include  <stdio.h>
+/**
+ * printf a line 
+ * return 0
+*/
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art, \n");
 	return (0);
